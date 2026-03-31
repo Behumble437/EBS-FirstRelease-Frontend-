a9a9343689144c549de5a024bf85f87aab363d50
xinyatang2022-boop <xinya.tang2022@gmail.com>
2026-03-31 08:48:26 -0400

    edit app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import EventsPage from "./pages/EventsPage";
+import EventFormPage from "./pages/EventFormPage";
+import AdminProtectedRoute from "./components/AdminProtectedRoute";
 
 export default function App() {
   return (
@@ -14,6 +17,25 @@ export default function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/events" element={<EventsPage />} />
+
+          <Route
+            path="/events/new"
+            element={
+              <AdminProtectedRoute>
+                <EventFormPage />
+              </AdminProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/events/edit/:id"
+            element={
+              <AdminProtectedRoute>
+                <EventFormPage />
+              </AdminProtectedRoute>
+            }
+          />
         </Routes>
       </main>
     </BrowserRouter>
